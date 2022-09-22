bd62321a608fa3b48d5e6e4185d945da5ee53d04
ccpar <ccpar@DESKTOP-4E40JFK.dhcp.wustl.edu>
2022-09-22 12:25:42 -0500

    hi

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,22 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("What is your value for n?");
+		int n = in.nextInt();
+		
+		boolean prime = sqrt(i);
+		
+		for (int i = 0; i < n; ++i)
+		{
+			
+		}
+	}
+
+}
